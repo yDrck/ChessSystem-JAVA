8ca44ac734b6a9e103210b4a918e7fa9a62acdb7
Rodrigo Talman <talman.profissional@gmail.com>
2021-03-15 18:17:08 -0300

    Implementing the matrix and testing the board

--- src/application/App.java
+++ src/application/App.java
@@ -1,12 +1,13 @@
 package application;
 
-import boardgame.Board;
+import chess.ChessMatch;
 
 public class App {
 
 	public static void main(String[] args) {
 
-		Board board = new Board(8, 8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
